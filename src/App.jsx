16dea581f271bc35a6b9485b03054287b036557d
Umesh Gayakwad <umesh.coder25@example.com>
2026-06-17 18:23:35 +0530

    add the gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Routes from "./Routes";
+import TargetCursor from "./components/TargetCursor";
 
 function App() {
   return (
-    <Routes />
+    <>
+      <TargetCursor
+        spinDuration={2}
+        hideDefaultCursor={true}
+        parallaxOn={true}
+        hoverDuration={0.2}
+      />
+
+      <Routes />
+    </>
   );
 }
 
